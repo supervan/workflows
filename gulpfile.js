b102d05fc38407d6bc5585630101f0a672e08812
Lyndon Bredenkamp <lyndon.bredenkamp@lri.co.uk>
2015-06-08 16:02:11 +0100

    Added Browserify

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,9 @@
 var gulp = require('gulp'),
 	gutil = require('gulp-util'),
 	coffee = require('gulp-coffee'),
-	concat = require('gulp-concat');
+	concat = require('gulp-concat'),
+	browserify = require('gulp-browserify')
+	;
 
 var coffeeSources = ['components/coffee/tagline.coffee'];
 var jsSources = [
@@ -22,5 +24,6 @@ gulp.task('coffee', function(){
 gulp.task('js', function(){
 	gulp.src(jsSources)
 		.pipe(concat('script.js'))
+		.pipe(browserify())
 		.pipe(gulp.dest('builds/development/js'))
 });
