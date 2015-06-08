f894d0d895deaa2c9a4a0b03c93ba7bc10a8e8a3
Lyndon Bredenkamp <lyndon.bredenkamp@lri.co.uk>
2015-06-08 18:31:21 +0100

    Added live reloading

--- builds/development/js/script.js
+++ builds/development/js/script.js
@@ -77,7 +77,7 @@ $ = require('jquery');
 
 (fill = function(item) {
   return $('.tagline').append("" + item);
-})('Creative minds in Art....');
+})('Creative minds in Art.....');
 
 fill;
 
--- components/scripts/tagline.js
+++ components/scripts/tagline.js
@@ -4,6 +4,6 @@ $ = require('jquery');
 
 (fill = function(item) {
   return $('.tagline').append("" + item);
-})('Creative minds in Art....');
+})('Creative minds in Art.....');
 
 fill;
