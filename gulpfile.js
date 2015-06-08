23ed6827641d75fc2055754f2ea75ab832ac6c15
Lyndon Bredenkamp <lyndon.bredenkamp@lri.co.uk>
2015-06-08 18:00:06 +0100

    Added gulp watch

--- gulpfile.js
+++ gulpfile.js
@@ -41,3 +41,11 @@ gulp.task('compass', function() {
 });
 
 gulp.task('default', ['coffee', 'js', 'compass'])
+
+gulp.task('watch', function(){
+  gulp.watch(coffeeSources, ['coffee']);
+  gulp.watch(jsSources, ['js']);
+  gulp.watch('components/sass/*.scss', ['compass']);
+});
+
+
